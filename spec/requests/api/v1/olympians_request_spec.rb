8edaf63f688fc1dc4e77f16ba63393ad7f8f4753
Zac Isaacson <isaacson.zm@gmail.com>
2020-03-03 12:15:42 -0700

    Complete spec for all olympians endpoint

--- spec/requests/api/v1/olympians_request_spec.rb
+++ spec/requests/api/v1/olympians_request_spec.rb
@@ -37,19 +37,19 @@ describe "Olympians API" do
 
     expect(response).to be_successful
 
-    # olympians = JSON.parse(response.body)
-    #
-    # expect(olympians['olympians'][0]['name']).to_eq(olympian_1.name)
-    # expect(olympians['olympians'][0]['team']).to_eq(olympian_1.team)
-    # expect(olympians['olympians'][0]['age']).to_eq(olympian_1.age)
-    # expect(olympians['olympians'][0]['sport']).to_eq(olympian_1.sport.name)
-    # expect(olympians['olympians'][0]['total_medals_won']).to_eq(olympian_1.total_medals_won)
-    #
-    # expect(olympians['olympians'][2]['name']).to_eq(olympian_3.name)
-    # expect(olympians['olympians'][2]['team']).to_eq(olympian_3.team)
-    # expect(olympians['olympians'][2]['age']).to_eq(olympian_3.age)
-    # expect(olympians['olympians'][2]['sport']).to_eq(olympian_3.sport.name)
-    # expect(olympians['olympians'][2]['total_medals_won']).to_eq(olympian_3.total_medals_won)
+    olympians = JSON.parse(response.body)
+
+    expect(olympians['olympians'][0]['name']).to eq(olympian_1.name)
+    expect(olympians['olympians'][0]['team']).to eq(olympian_1.team)
+    expect(olympians['olympians'][0]['age']).to eq(olympian_1.age)
+    expect(olympians['olympians'][0]['sport']).to eq(olympian_1.sport.name)
+    expect(olympians['olympians'][0]['total_medals_won']).to eq(olympian_1.total_medals_won)
+
+    expect(olympians['olympians'][2]['name']).to eq(olympian_3.name)
+    expect(olympians['olympians'][2]['team']).to eq(olympian_3.team)
+    expect(olympians['olympians'][2]['age']).to eq(olympian_3.age)
+    expect(olympians['olympians'][2]['sport']).to eq(olympian_3.sport.name)
+    expect(olympians['olympians'][2]['total_medals_won']).to eq(olympian_3.total_medals_won)
 
   end
 end
