e2407020a0894c779971e10470df9e415bc8ae8c
Zac Isaacson <isaacson.zm@gmail.com>
2020-03-03 12:04:04 -0700

    Create total_medals_won model spec and functionality

--- app/models/olympian.rb
+++ app/models/olympian.rb
@@ -7,7 +7,11 @@ class Olympian < ApplicationRecord
                       :team
 
   belongs_to :sport
-  
+
   has_many :olympian_events
   has_many :events, through: :olympian_events
+
+  def total_medals_won
+    olympian_events.where.not(medal: 'NA').length
+  end
 end
