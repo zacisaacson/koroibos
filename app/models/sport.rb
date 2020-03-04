9377ae836ddd93b67c9add3402fa40db664de062
Zac Isaacson <isaacson.zm@gmail.com>
2020-03-03 21:56:28 -0700

    Add sport model method to pluck name and sort

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,4 +1,15 @@
 class Sport < ApplicationRecord
   validates_presence_of :name
 
+  has_many :olympians
+  has_many :events
+
+  def self.sorted_name
+    order(:name)
+  end
+
+  def event_name
+    events.pluck(:name)
+  end
+
 end
