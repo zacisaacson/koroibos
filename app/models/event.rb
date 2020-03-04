8c8cf485b2475eb204227ac56b2dac52c32cff80
Zac Isaacson <isaacson.zm@gmail.com>
2020-03-04 01:02:20 -0700

    Create event model method for medaled olympians

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,8 @@ class Event < ApplicationRecord
 
   has_many :olympian_events
   has_many :olympians, through: :olympian_events
+
+  def medaled_olympians
+    olympian_events.where(event_id: id).where.not(medal: "NA")
+  end
 end
