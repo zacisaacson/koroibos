a8ba29df7342a0c96c3cf96f004fef2b91557706
Zac Isaacson <isaacson.zm@gmail.com>
2020-03-04 00:58:52 -0700

    Create route for medalists endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get '/olympians', to: 'olympians#index'
       get '/olympian_stats', to: 'olympian_stats#show'
-      get '/events', to: 'events#show'
+      get '/events', to: 'events#index'
+      get '/events/:id/medalists', to: 'events#show'
     end
   end
 end
